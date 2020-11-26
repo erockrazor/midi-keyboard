45f404c743f157952a45e00b70f9ed63fea1b09b
Eric Gieg <eric.gieg@proplogix.com>
2020-11-26 11:25:44 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  if (navigator.requestMIDIAccess) {
+    console.log("This browser supports WebMIDI!");
+  } else {
+    console.log("WebMIDI is not supported in this browser.");
+  }
+  /* navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure); */
+
+  /* function onMIDISuccess(midiAccess) { */
+  /*   console.log(midiAccess); */
+
+  /*   var inputs = midiAccess.inputs; */
+  /*   var outputs = midiAccess.outputs; */
+  /* } */
+
+  /* function onMIDIFailure() { */
+  /*   console.log("Could not access your MIDI devices."); */
+  /* } */
+</script>
+
+<style>
+</style>
+
+<main>
+  <h2>Midi Keyboard App</h2>
+</main>
